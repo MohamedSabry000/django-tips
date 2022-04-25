2c233244bdeb0d4c0a30dc4eb18497fe412b1eed
Mohamed Sabry <30502846+Mohamedfadah@users.noreply.github.com>
2022-04-25 16:03:21 +0200

    file structure and use layouts for pages

--- Day 3/django_project/djproj/djapp/forms.py
+++ Day 3/django_project/djproj/djapp/forms.py
@@ -5,6 +5,12 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ('fname', 'lname', 'age', 'student_track')
+        widgets = {
+            'fname': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
+            'lname': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
+            'age': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Age'}),
+            'student_track': forms.Select(attrs={'class': 'form-control'}),
+        }
         
 class TrackForm(forms.ModelForm):
     class Meta:
