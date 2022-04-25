449abe9d7d5a1194b5dc4aea04221523d418a355
Mohamed Sabry <30502846+Mohamedfadah@users.noreply.github.com>
2022-04-25 16:48:16 +0200

    API Functionalities using djanngoresframework

--- Day 3/django_project/djproj/djapp/urls.py
+++ Day 3/django_project/djproj/djapp/urls.py
@@ -7,4 +7,18 @@ urlpatterns = [
     path('add/', views.addStudent, name='add'),
     path('edit/<st_id>', views.editStudent, name='edit'),
     
+    # rest_framework path
+    path('api-all/', views.api_all_students, name='api-all'),
+    path('api-one/<st_id>', views.api_one_student, name='api-one'),
+    path('api-add/', views.api_add_student, name='api-add'),
+    path('api-edit/<st_id>', views.api_edit_student, name='api-edit'),
+    path('api-del/<st_id>', views.api_del_student, name='api-del'),
+
+
+# {
+#         "fname": "wael",
+#         "lname": "NoName",
+#         "age": 2,
+#         "student_track": 1
+# }
 ]
--- Day 3/django_project/djproj/djapp/views.py
+++ Day 3/django_project/djproj/djapp/views.py
@@ -3,6 +3,47 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from .models import Student, Track
 from .forms import StudentForm
+
+# rest_framework imports
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+# rest_framework views here
+
+@api_view(['GET'])
+def api_all_students(request):
+    all_students = Student.objects.all()
+    serializer = StudentSerializer(all_students, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def api_one_student(request, st_id):
+    st = Student.objects.get(id=st_id)
+    serializer = StudentSerializer(st, many=False)
+    return Response(serializer.data)
+    
+@api_view(['POST'])
+def api_add_student(request):
+    st_ser = StudentSerializer(data=request.data)
+    if st_ser.is_valid():
+        st_ser.save()
+        return redirect('api-all')
+
+@api_view(['POST'])
+def api_edit_student(request, st_id):
+    st = Student.objects.get(id=st_id)
+    st_ser = StudentSerializer(instance=st, data=request.data)
+    if st_ser.is_valid():
+        st_ser.save()
+        return redirect('api-all')
+
+@api_view(['DELETE'])
+def api_del_student(request, st_id):
+    st = Student.objects.get(id=st_id)
+    st.delete()
+    return Response('Student deleted successfully')
+
+
 # Create your views here.
 def home(request):
     all_students = Student.objects.all()
